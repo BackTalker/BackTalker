ab0e5e83f92949d83c18ea2a5656394cbae2c176
mca183 <mca183@sfu.ca>
2019-10-06 14:34:41 -0700

    weather feature is still working in progress, need to wait for my api key to activate

---
+++ modules/weather.py
@@ -0,0 +1,59 @@
+'''
+
+ip: "154.20.144.52"
+type: "ipv4"
+continent_code: "NA"
+continent_name: "North America"
+country_code: "CA"
+country_name: "Canada"
+region_code: "BC"
+region_name: "British Columbia"
+city: "Richmond"
+zip: V6V 1A1
+latitude: 49.169960021972656
+longitude: -123.09098815917969
+location: Object {}
+time_zone: Object{}
+currency: Object{}
+connection: Object{}
+security: Object{}
+'''
+
+import requests
+import json
+
+send_url = 'http://api.ipstack.com/154.20.144.52?access_key=4e9f9a4d9d5411bd4c9a9f171d6a42ea'
+r = requests.get(send_url)
+j = json.loads(r.text)
+
+city = j['city']
+region = j['region_code']
+
+print(city)
+print(region)
+
+
+from pyowm import OWM
+API_keys = '23ae1c41e14442adefb138bcf1efe3c6'
+owm = OWM(API_keys)
+
+obs = owm.weather_at_place('Richmon,BC')
+
+print(obs)
+
+
+
+'''
+import requests
+import json
+
+send_url = 'http://api.ipstack.com/154.20.144.52?access_key=4e9f9a4d9d5411bd4c9a9f171d6a42ea'
+r = requests.get(send_url)
+j = json.loads(r.text)
+
+lat = j['latitude']
+lon = j['longitude']
+
+city = j['city']
+region = j['region_code']
+'''
