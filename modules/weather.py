247af843b14f70ac8b081037c6bd4e6d7db55282
Michael Chang <mchang0926@gmail.com>
2019-10-08 13:47:36 -0700

    the bot can now output current weather

--- modules/weather.py
+++ modules/weather.py
@@ -1,59 +1,41 @@
 '''
+weather.py
+By: Michael Chang
+Check weather at your current location
 
-ip: "154.20.144.52"
-type: "ipv4"
-continent_code: "NA"
-continent_name: "North America"
-country_code: "CA"
-country_name: "Canada"
-region_code: "BC"
-region_name: "British Columbia"
-city: "Richmond"
-zip: V6V 1A1
-latitude: 49.169960021972656
-longitude: -123.09098815917969
-location: Object {}
-time_zone: Object{}
-currency: Object{}
-connection: Object{}
-security: Object{}
 '''
 
-import requests
 import json
-
-send_url = 'http://api.ipstack.com/154.20.144.52?access_key=4e9f9a4d9d5411bd4c9a9f171d6a42ea'
-r = requests.get(send_url)
-j = json.loads(r.text)
-
-city = j['city']
-region = j['region_code']
-
-print(city)
-print(region)
-
-# https://pyowm.readthedocs.io/en/latest/usage-examples-v2/weather-api-usage-examples.html#owm-weather-api-version-2-5-usage-examples
+import requests
 from pyowm import OWM
-API_keys = '23ae1c41e14442adefb138bcf1efe3c6'
-owm = OWM(API_keys)
+from socket import *
 
-obs = owm.weather_at_place('Richmon,BC')
+def currentWeather():
+    IPAddr = gethostbyname(gethostname())
+    send_url = 'http://api.ipstack.com/{}?access_key=4e9f9a4d9d5411bd4c9a9f171d6a42ea'.format(IPAddr)
+    r = requests.get(send_url)
+    j = json.loads(r.text)
 
-print(obs)
+    city = j['city']
+    region = j['region_code']
+    country = j['country_code']
+
+    # print(city)
+    # print(region)
+
+    # https://pyowm.readthedocs.io/en/latest/usage-examples-v2/weather-api-usage-examples.html#owm-weather-api-version-2-5-usage-examples
+    API_keys = '23ae1c41e14442adefb138bcf1efe3c6'
+    owm = OWM(API_keys)
+    locateMe = '{},{}'.format(city,country)
+    obs = owm.weather_at_place(locateMe)
+    # print(obs)   
+    w = obs.get_weather()
+
+    response = "The weather is now {}°C/{}°F at {}, {} with {}".format(w.get_temperature(unit='celsius')['temp'],
+                                                                      w.get_temperature('fahrenheit')['temp'],
+                                                                      city, region, w.get_detailed_status())
+    # print(response)
+    return response
+    
 
 
-
-'''
-import requests
-import json
-
-send_url = 'http://api.ipstack.com/154.20.144.52?access_key=4e9f9a4d9d5411bd4c9a9f171d6a42ea'
-r = requests.get(send_url)
-j = json.loads(r.text)
-
-lat = j['latitude']
-lon = j['longitude']
-
-city = j['city']
-region = j['region_code']
-'''
