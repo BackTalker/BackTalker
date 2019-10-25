20947bc23d056c909883f71b2764caa7343d5f07
Michael Chang <mchang0926@gmail.com>
2019-10-25 00:00:45 -0700

    Put my token in there, so its easier to run it

--- backtalker.py
+++ backtalker.py
@@ -1,53 +1,95 @@
-import time
 import os
-from random import randrange
+import time
+import re
+import datetime as dt
 from slackclient import SlackClient
 
-# Cont variable
-SLACK_CLIENT = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-BOT_ID = SLACK_CLIENT.api_call("auth.test")["user_id"]
-CHANNEL_LIST = SLACK_CLIENT.api_call("channels.list").get("channels", [])
-ANSWER_LIST = [
-    "Umm, interesting I will ask my master for its meaning",
-    "I think it cool",
-    "Howdy!",
-    "What's up?",
-    "I see",
-    "Anythings else?",
-    "I'm done!!!"
-]
+from modules.all import *
 
-def findChannelsID(channelName, channelList):
+# instantiate Slack client
+SLACK_BOT_TOKEN = 'xoxb-782056409184-787065217654-pSbkpFs7UmUmz3wjfXXdie3S'
+
+# slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+slack_client = SlackClient(SLACK_BOT_TOKEN)
+# backtalker's user ID in Slack: value is assignn
+backtalker_id = None
+
+# constants
+RTM_READ_DELAY = 1
+EXAMPLE_COMMAND = "do"
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+
+def parse_bot_commands(slack_events):
     """
+        Parses a list of events coming from the Slack RTM API to find bot commands.
+        If a bot command is found, this function returns a tuple of command and channel.
+        If its not found, then this function returns None, None.
     """
-    match = [ch.get("id") for ch in channelList if ch.get("name") == channelName]
-    return match[0]
+    for event in slack_events:
+        if event["type"] == "message" and not "subtype" in event:
+            user_id, message = parse_direct_mention(event["text"])
+            if user_id == backtalker_id:
+                return message, event["channel"]
+    return None, None
+
+
+def parse_direct_mention(message_text):
+    """
+        Finds a direct mention (a mention that is at the beginning) in message text
+        and returns the user ID which was mentioned. If there is no direct mention, returns None
+    """
+    matches = re.search(MENTION_REGEX, message_text)
+    # 1st gourp has username, 2nd group called remaining message
+    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
+def handle_command(command, channel):
+    """
+        Executes bot command if the command is known
+    """
+    default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
+
+    response = None
+
+    if command.startswith(EXAMPLE_COMMAND):
+        response = "Sure...write some code then I can do that!"
+    elif command.startswith("date"):
+        response = currentDate()
+    elif command.startswith("time"):
+        response = currentTime()
+    elif command.startswith("your my best friend") or command.startswith("you are my best friend"):
+        response = "Thanks so much, buddy!!! \n Your the best!!"
+    elif command.startswith("hello") or command.startswith("hi") or command.startswith("hey"):
+        response = "Hello, My name is BackTalker"
+    elif command.startswith("thanks") or command.startswith("thank you"):
+        response = "Your Welcome"
+    elif command.startswith("math"):
+        problem = command[4:]
+        response = "The answer for {} is {}".format(problem, str(eval(problem)))
+    elif command.startswith("say something"):
+        response = compliments()    
+    elif command.startswith("weather"):
+        response = currentWeather()
+    elif command.startswith("cmpt371"):
+        word = command[8:]
+        response = cmpt371(word)
+
+
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=response or default_response
+    )
 
 if __name__ == "__main__":
-    """
-    """
-    if (SLACK_CLIENT.rtm_connect(with_team_state=False)):
-        print("I'm online rn, please order me...")
-        responseChannels = findChannelsID("general", CHANNEL_LIST)
-        SLACK_CLIENT.api_call(
-            "chat.postMessage",
-            channel=responseChannels,
-            text="Hello, My name is BackTalker",
-        )
+    if slack_client.rtm_connect(with_team_state=False):
+        print("Backtalker is connected and running!!")
+
+        backtalker_id = slack_client.api_call("auth.test")["user_id"]
         while True:
-            data = SLACK_CLIENT.rtm_read()
-            if not data:
-                continue
-            else:
-                newData = data[0]
-                print(newData)
-                if (newData.get("type") == "message") and (newData.get("subtype") != "bot_message"):
-                    SLACK_CLIENT.api_call(
-                        "chat.postMessage",
-                        channel=responseChannels,
-                        text=ANSWER_LIST[randrange(len(ANSWER_LIST))]
-                    )
-            
-            time.sleep(1)
+            command, channel = parse_bot_commands(slack_client.rtm_read())
+            if command:
+                handle_command(command, channel)
+            time.sleep(RTM_READ_DELAY)
     else:
-        print("Something wrong, please check your internet connection!")
+        print("Connection is not working. Please check ur connection.")
